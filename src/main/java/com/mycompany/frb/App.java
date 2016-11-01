11c552c905e673006a4a89819e15eaffb93ac7ba
Colin But <colinbut@users.noreply.github.com>
2016-11-01 20:52:25 +0000

    cleaned up maven generated code

--- src/main/java/com/mycompany/frb/App.java
+++ src/main/java/com/mycompany/frb/App.java
@@ -1,13 +1,12 @@
 package com.mycompany.frb;
 
 /**
- * Hello world!
- *
+ * Application
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args){
+        System.out.println("Application");
     }
+
 }
