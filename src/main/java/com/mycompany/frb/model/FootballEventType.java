93d1a35fbd391beb36aa3e21cb32184fd692322f
Colin But <colinbut@users.noreply.github.com>
2016-11-03 18:13:14 +0000

    some model improvements

--- src/main/java/com/mycompany/frb/model/FootballEventType.java
+++ src/main/java/com/mycompany/frb/model/FootballEventType.java
@@ -7,7 +7,20 @@ package com.mycompany.frb.model;
 
 public enum FootballEventType {
 
-    GOAL_SCORED,
-    YELLOW_CARD,
-    RED_CARD
+    GOAL_SCORED ("GOAL SCORED"),
+    YELLOW_CARD ("YELLOW CARD"),
+    RED_CARD ("RED CARD"),
+    FULL_TIME ("FT"),
+    HALF_TIME ("HT"),
+    GOAL_LATEST ("GL");
+
+    private String code;
+
+    FootballEventType(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
